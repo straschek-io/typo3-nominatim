e6abdecf052686471046828438c372c16ffb6bc3
Michael Straschek <m@straschek.io>
2018-10-01 19:14:33 +0200

    [FEATURE] Add signals before and after nominatim search

--- Classes/Service/GeocodeService.php
+++ Classes/Service/GeocodeService.php
@@ -7,6 +7,9 @@ use TYPO3\CMS\Core\Cache\CacheManager;
 use TYPO3\CMS\Core\Cache\Exception\NoSuchCacheException;
 use TYPO3\CMS\Core\Cache\Frontend\FrontendInterface;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Object\ObjectManager;
+use TYPO3\CMS\Extbase\Object\ObjectManagerInterface;
+use TYPO3\CMS\Extbase\SignalSlot\Dispatcher;
 
 class GeocodeService
 {
@@ -14,11 +17,29 @@ class GeocodeService
     const FETCH_STATUS_NORESULT = 'noresult';
 
     protected $url = 'http://nominatim.openstreetmap.org/';
+
+    /**
+     * @var Nominatim
+     */
     protected $nominatim = null;
 
-    public function __construct()
-    {
+    /**
+     * @var Dispatcher
+     */
+    protected $signalSlotDispatcher = null;
+
+    /**
+     * @var ObjectManagerInterface
+     */
+    private $objectManager;
+
+    public function __construct(
+        ObjectManagerInterface $objectManager,
+        Dispatcher $signalSlotDispatcher
+    ) {
+        $this->objectManager = $objectManager ?: GeneralUtility::makeInstance(ObjectManager::class);
         $this->nominatim = GeneralUtility::makeInstance(Nominatim::class, $this->url);
+        $this->signalSlotDispatcher = $signalSlotDispatcher ?: $this->objectManager->get(Dispatcher::class);
     }
 
     /**
@@ -72,51 +93,55 @@ class GeocodeService
         }
     }
 
-	/**
-	 * @param string $searchString
-	 * @param array $countryCodes
-	 * @return array
-	 * @throws \maxh\Nominatim\Exceptions\InvalidParameterException
-	 */
-	public function getCoordinatesForSearchString(string $searchString, $countryCodes = []): array
-	{
-		try {
-			$cache = $this->initializeCache();
-			$cacheKey = 'nominatim-' . strtolower(str_replace(
-					' ',
-					'-',
-					preg_replace('/[^0-9a-zA-Z ]/m', '', trim($searchString))
-				)) . '-' . implode('-', $countryCodes);
+    /**
+     * @param string $searchString
+     * @param array $countryCodes
+     * @throws \TYPO3\CMS\Extbase\SignalSlot\Exception\InvalidSlotException
+     * @throws \TYPO3\CMS\Extbase\SignalSlot\Exception\InvalidSlotReturnException
+     * @throws \maxh\Nominatim\Exceptions\InvalidParameterException
+     * @return array
+     */
+    public function getCoordinatesForSearchString(string $searchString, $countryCodes = []): array
+    {
+        try {
+            $cache = $this->initializeCache();
+            $cacheKey = 'nominatim-' . strtolower(str_replace(
+                    ' ',
+                    '-',
+                    preg_replace('/[^0-9a-zA-Z ]/m', '', trim($searchString))
+                )) . '-' . implode('-', $countryCodes);
 
-			if (!$cache->has($cacheKey)) {
-				$search = $this->nominatim->newSearch();
-				$search->query($searchString);
-				$search->limit(1);
-				foreach ($countryCodes as $countryCode) {
-					$search->countryCode($countryCode);
-				}
-				$result = $this->nominatim->find($search);
-				if (!empty($result[0])) {
-					$coordinates = [
-						'status' => self::FETCH_STATUS_SUCCESS,
-						'latitude' => $result[0]['lat'],
-						'longitude' => $result[0]['lon'],
-					];
-				} else {
-					$coordinates = [
-						'status' => self::FETCH_STATUS_NORESULT,
-					];
-				}
-				$coordinates['queryString'] = $search->getQueryString();
-				$cache->set($cacheKey, $coordinates, []);
-			} else {
-				$coordinates = $cache->get($cacheKey);
-			}
-			return $coordinates;
-		} catch (NominatimException $e) {
-			throw new \RuntimeException($e->getMessage(), $e->getCode());
-		}
-	}
+            if (!$cache->has($cacheKey)) {
+                $this->signalSlotDispatcher->dispatch(__CLASS__, 'nominatimSearchPreProcess', [&$searchString]);
+                $search = $this->nominatim->newSearch();
+                $search->query($searchString);
+                $search->limit(1);
+                foreach ($countryCodes as $countryCode) {
+                    $search->countryCode($countryCode);
+                }
+                $result = $this->nominatim->find($search);
+                if (!empty($result[0])) {
+                    $coordinates = [
+                        'status' => self::FETCH_STATUS_SUCCESS,
+                        'latitude' => $result[0]['lat'],
+                        'longitude' => $result[0]['lon'],
+                    ];
+                } else {
+                    $coordinates = [
+                        'status' => self::FETCH_STATUS_NORESULT,
+                    ];
+                }
+                $coordinates['queryString'] = $search->getQueryString();
+                $cache->set($cacheKey, $coordinates, []);
+            } else {
+                $coordinates = $cache->get($cacheKey);
+            }
+            $this->signalSlotDispatcher->dispatch(__CLASS__, 'nominatimSearchPostProcess', [&$searchString, &$coordinates]);
+            return $coordinates;
+        } catch (NominatimException $e) {
+            throw new \RuntimeException($e->getMessage(), $e->getCode());
+        }
+    }
 
     protected function initializeCache(): FrontendInterface
     {
